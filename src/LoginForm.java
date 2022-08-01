a7f3dd181897c36d326159d15cc1e21a34192743
Jhon <jhon-3316@hotmail.com>
2022-08-01 09:17:36 -0500

    version final Lunes

--- src/LoginForm.java
+++ src/LoginForm.java
@@ -2,9 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
+import java.sql.*;
 
 public class LoginForm extends JDialog{
     private JTextField emailFT;
@@ -46,13 +44,24 @@ public class LoginForm extends JDialog{
         cancelButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                dispose();
             }
         });
+        setVisible(true);
     }
 
     public static void main(String[] args) {
         LoginForm loginForm = new LoginForm(null);
+        User user = loginForm.user;
+        if(user!=null){
+            System.out.println("Autenticación correcta: "+user.nombre);
+            System.out.println("Email: "+user.email);
+            System.out.println("Celular:"+user.celular);
+            System.out.println("Dirección: "+user.direccion);
+        }
+        else {
+            System.out.println("Autenticación fallida");
+        }
     }
 
     //elementos de autenticacion
@@ -60,12 +69,29 @@ public class LoginForm extends JDialog{
 
     private User getAuthenticationUser(String email, String password){
         User user = null;
-        
+
         final String DB_URL="jdbc:mysql://localhost/mitienda?serverTimezon=UTC";
         final String USERNAME="rooT";
         final String PASSWORD="";
         try{
             Connection conn= DriverManager.getConnection(DB_URL,USERNAME, PASSWORD);
+            Statement stnt=conn.createStatement();
+            String sql="SELECT * FROM users WHERE email=? AND password=?";
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setString(1,email);
+            preparedStatement.setString(2, password);
+
+            ResultSet resultSet= preparedStatement.executeQuery();
+
+            if(resultSet.next()){
+                user = new User();
+                /*user.nombre = resultSet.getString(nombre);
+                user.email=resultSet.getString(email);
+                user.celular=resultSet.getString(celular);
+                user.direccion=resultSet.getString(direccion);
+                user.password=resultSet.getString(password);*/
+            }
+
         } catch (Exception e){
             e.printStackTrace();
         }
