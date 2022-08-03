762bdb24fd8aeb7a43ed315cff079fa2e81f7fcc
Jhon <jhon-3316@hotmail.com>
2022-08-02 20:47:48 -0500

    Ventana nueva Perfil

--- src/Login/User.java
+++ src/Login/User.java
@@ -1,11 +1,14 @@
+package Login;
+
 import javax.swing.*;
 
-public class User extends JDialog {
+public class User extends JDialog{
     public String nombre;
     public String email;
     public String celular;
     public String direccion;
     public String password;
+
 
 
 }
